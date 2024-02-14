82b74385ad33b29664f77ca2316f89d4972f27b7
infibrocco <towsifhassanz3@gmail.com>
2024-02-14 12:40:41 +0600

    Revert "chore: remove FTB modpack support"
    This reverts commit ff07714e8c955003bb3deec84ec6cce6fc3ef5e6.

--- launcher/ui/dialogs/NewInstanceDialog.cpp
+++ launcher/ui/dialogs/NewInstanceDialog.cpp
@@ -55,7 +55,16 @@
 #include <QValidator>
 #include <utility>
 
+<<<<<<< HEAD
 #include "ui/pages/modplatform/CustomPage.h"
+=======
+#include "ui/widgets/PageContainer.h"
+#include "ui/pages/modplatform/VanillaPage.h"
+#include "ui/pages/modplatform/atlauncher/AtlPage.h"
+#include "ui/pages/modplatform/ftb/FtbPage.h"
+#include "ui/pages/modplatform/legacy_ftb/Page.h"
+#include "ui/pages/modplatform/flame/FlamePage.h"
+>>>>>>> parent of ff07714e (chore: remove FTB modpack support)
 #include "ui/pages/modplatform/ImportPage.h"
 #include "ui/pages/modplatform/atlauncher/AtlPage.h"
 #include "ui/pages/modplatform/flame/FlamePage.h"
@@ -159,6 +168,7 @@ QList<BasePage*> NewInstanceDialog::getPages()
     pages.append(new AtlPage(this));
     if (APPLICATION->capabilities() & Application::SupportsFlame)
         pages.append(new FlamePage(this));
+    pages.append(new FtbPage(this));
     pages.append(new LegacyFTB::Page(this));
     pages.append(new FTBImportAPP::ImportFTBPage(this));
     pages.append(new ModrinthPage(this));
